dea2b4bc30adebf92d6d0aacc12d4be7574abbee
cosi <corinna-reithmeier@web.de>
2022-03-11 03:58:36 +0100

    GameManager created

---
+++ Dont Panic/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState GameState;
+
+    void Awake(){
+        Instance = this;
+    }
+
+    void Start(){
+        ChangeState(GameState.GenerateGrid);
+
+    }
+
+    public void ChangeState(GameState newState){
+        GameState = newState;
+        switch (newState){
+            case GameState.GenerateGrid:
+             break;
+             case GameState.SpawnPlayers:
+             break;
+             case GameState.SpawnPatrol:
+             break;
+             case GameState.PlayerOneTurn:
+             break;
+             case GameState.PlayerTwoTurn:
+             break;
+             case GameState.PatrolTurn:
+             break;
+             // default: throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+}
+
+public enum GameState {
+    GenerateGrid = 0,
+    SpawnPlayers = 1,
+    SpawnPatrol = 2,
+    PlayerOneTurn = 3,
+    PlayerTwoTurn = 4,
+    PatrolTurn = 5
+}
