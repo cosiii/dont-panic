bc5c9cad4fe255297e87ab172764e872a93a2e53
cosi <corinna-reithmeier@web.de>
2022-03-16 17:08:42 +0100

    updated UnitManager

--- Dont Panic/Assets/Scripts/Managers/UnitManager.cs
+++ Dont Panic/Assets/Scripts/Managers/UnitManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class UnitManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static UnitManager Instance;
+    private List<ScriptableUnit> units;
+    void Awake(){
+        Instance = this;
+        // goes through Units Folder and look thorugh all the subfolders fpr any type of scriptable unit
+        units = Resources.LoadAll<ScriptableUnit>("Units").ToList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SpawnPlayers(){
+        var playerCount =2;
+        for (int i = 0; i < playerCount; i++)
+        {
+            var randomPrefab = GetRandomUnit<BasePlayer>(Faction.Player);
+            var spawnedHero = Instantiate(randomPrefab);
+        }
+    }
+
+
+    private T GetRandomUnit<T>(Faction faction) where T : BaseUnit {
+        // going thorugh our list, wanting all the units of the faction we*re telling it, randomly shuffling them around (why?), take the first and just get the Prefab
+        return (T)units.Where(u => u.Faction == faction).OrderBy( o => Random.value).First().UnitPrefab;
     }
 }
