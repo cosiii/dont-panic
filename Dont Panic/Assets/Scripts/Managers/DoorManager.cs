65bfdc849a73ed982f4c843b285654d1aec1d804
cosi <corinna-reithmeier@web.de>
2022-04-05 03:15:16 +0200

    Door generates Items and shows what you need

--- Dont Panic/Assets/Scripts/Managers/DoorManager.cs
+++ Dont Panic/Assets/Scripts/Managers/DoorManager.cs
@@ -9,23 +9,32 @@ public class DoorManager : MonoBehaviour
   public string lastVisitedDoor;
 
   public string[] keyItems1, keyItems2, keyItems3, keyItems4, keyItems5;
-
+  
+public string[] RandomItems = {"Item1","Item2", "Item3", "Item4", "Item5", "Item6", "Item7" };
   void Awake(){
       Instance = this;
+      for (int i = 0; i < 2; i++)
+      {
+      keyItems1[i] = RandomItems[Random.Range(0,RandomItems.Length)];
+      keyItems2[i] = RandomItems[Random.Range(0,RandomItems.Length)];
+      keyItems3[i] = RandomItems[Random.Range(0,RandomItems.Length)];
+      keyItems4[i] = RandomItems[Random.Range(0,RandomItems.Length)];
+      keyItems5[i] = RandomItems[Random.Range(0,RandomItems.Length)];
+      }
   }
 
   public void DoorCollision(){
         //Debug.Log(lastVisitedDoor);
         if (lastVisitedDoor == "door1"){
-            Debug.Log("we are in front of door1");
+            Debug.Log("door1" + keyItems1[0]);
         } else if (lastVisitedDoor == "door2"){
-            Debug.Log("now door2");
+            Debug.Log("door2" + keyItems2[0]);
         }  else if (lastVisitedDoor == "door3"){
-            Debug.Log("now door3");
+            Debug.Log("door3" + keyItems3[0]);
         } else if (lastVisitedDoor == "door4"){
-            Debug.Log("now door 4");
+            Debug.Log("door4" + keyItems4[0]);
         } else if (lastVisitedDoor == "door5"){
-            Debug.Log("now door 5");
+            Debug.Log("door5" + keyItems5[0]);
         }
       // jedem Door zwei Items zuordnen, evtl als String ausgeben
       
