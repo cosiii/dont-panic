e913a479ac908000186ad61b7ff21b69c3959779
cosi <corinna-reithmeier@web.de>
2022-04-05 03:43:56 +0200

    DoorManager knows if you have the right items

--- Dont Panic/Assets/Scripts/Managers/DoorManager.cs
+++ Dont Panic/Assets/Scripts/Managers/DoorManager.cs
@@ -25,18 +25,52 @@ public string[] RandomItems = {"Item1","Item2", "Item3", "Item4", "Item5", "Item
 
   public void DoorCollision(){
         //Debug.Log(lastVisitedDoor);
+         
+
+
         if (lastVisitedDoor == "door1"){
             Debug.Log("door1" + keyItems1[0]);
+            SearchItem(keyItems1[0], keyItems1[1]);
         } else if (lastVisitedDoor == "door2"){
             Debug.Log("door2" + keyItems2[0]);
+            SearchItem(keyItems2[0],keyItems2[1]);
         }  else if (lastVisitedDoor == "door3"){
             Debug.Log("door3" + keyItems3[0]);
+            SearchItem(keyItems3[0], keyItems3[1]);
         } else if (lastVisitedDoor == "door4"){
             Debug.Log("door4" + keyItems4[0]);
+            SearchItem(keyItems4[0], keyItems4[1]);
         } else if (lastVisitedDoor == "door5"){
             Debug.Log("door5" + keyItems5[0]);
+            SearchItem(keyItems5[0], keyItems5[1]);
         }
       // jedem Door zwei Items zuordnen, evtl als String ausgeben
       
+  }
+  public void SearchItem(string stringToCheck, string itemToHaveNext){
+      if(GameManager.Instance.GameState == GameState.Player1Turn){
+          foreach (string x in InventoryManager.Instance.inventoryPlayerOne)
+         {
+             if (x.Equals (stringToCheck))
+             {
+                 Debug.Log("you, player one has this item, the next item is " + itemToHaveNext);
+             }
+
+             if (x.Equals (itemToHaveNext))
+             {
+                 Debug.Log("xou also have the next item");
+             }
+         }
+      }
+      
+    if(GameManager.Instance.GameState == GameState.Player2Turn){
+        foreach (string x in InventoryManager.Instance.inventoryPlayerTwo)
+         {
+             if (x.Equals (stringToCheck))
+             {
+                 Debug.Log("you, player two has this item");
+             }
+         }
+    }
   }
 }
