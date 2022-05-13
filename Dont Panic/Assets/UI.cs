dfa9e7a5387be668a111212393183b668ce7d4e0
cosi <corinna-reithmeier@web.de>
2022-05-13 10:07:08 +0200

    Timer created

---
+++ Dont Panic/Assets/UI.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{	
+ public Text TimerText; 
+ public bool playing;
+ private float Timer;
+
+ void Update () {
+
+ 	if(playing == true){
+  
+	  Timer += Time.deltaTime;
+	  int minutes = Mathf.FloorToInt(Timer / 60F);
+	  int seconds = Mathf.FloorToInt(Timer % 60F);
+	  int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+	  TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");
+	}
+
+  }
+
+}
