0a56c1d791f851405e9e8edfa32610d117cb38b3
cosi <corinna-reithmeier@web.de>
2022-07-20 15:48:10 +0200

    texts modified

--- Dont Panic/Assets/Scripts/Managers/UIManager.cs
+++ Dont Panic/Assets/Scripts/Managers/UIManager.cs
@@ -51,7 +51,7 @@ void Start(){
 	  int minutes = Mathf.FloorToInt(Timer / 60F);
 	  int seconds = Mathf.FloorToInt(Timer % 60F);
 	  int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
-	  TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");
+	  TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00");
 	}
 
   }
